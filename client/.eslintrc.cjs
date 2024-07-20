79dca4e5253a2362b54ddb224a438de246ee729b
Samuel Gomez <samuelgomez4010@gmail.com>
2024-07-19 20:08:59 -0400

    chore: Add Vite configuration and TypeScript files

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.app.json',
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/function-component-definition': 'off',
+    '@typescript-eslint/consistent-type-imports': 'error',
+    'react/prop-types': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
+  },
+}
---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  const handleClick = () => {}
+  return (
+    <button
+      type="button"
+      onClick={handleClick}>
+      {' '}
+      Connect to Spotify
+    </button>
+  )
+}
+
+export default App
