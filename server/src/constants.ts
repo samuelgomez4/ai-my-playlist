07556dab4bfdff4fe92b8d27c845dd20e7516767
Samuel Gomez <samuelgomez4010@gmail.com>
2024-07-26 21:58:58 -0400

    add comments to constants file

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -4,6 +4,7 @@ import { getEnv } from './utils/getEnv'
 export const CLIENT_ID = 'cd601ca9deab4d55ad4a4214d7146d58'
 export const REDIRECT_URI = 'http://localhost:5173'
 export const AUTHORIZE_ENDPOINT = 'https://accounts.spotify.com/authorize?'
+// getEnv method ensures that a string is returned
 export const CLIENT_SECRET = getEnv('CLIENT_SECRET')
 
 // scope is used to specify the access level which the user has to authorize
