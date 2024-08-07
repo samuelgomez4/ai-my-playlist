11db2253f75ae7f02596c0a7a248e729c0e10488
Samuel Gomez <samuelgomez4010@gmail.com>
2024-08-07 04:50:00 -0400

    update SideBar component with new icons and functionality to delete api key and token

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -1,21 +1,31 @@
-import { Music2Icon, SettingsIcon } from './icons/icons'
+import { KeyRound, LogOut } from 'lucide-react'
 import { Button } from './ui/button'
 
 export function SideBar() {
   return (
-    <aside className="flex flex-col border-r bg-background py-4 px-2">
+    <aside className="flex flex-col border-r bg-background py-8 px-2">
       <Button
+        title="Remove Gemini API Key"
         variant="ghost"
         size="icon"
-        className="rounded-full">
-        <Music2Icon className="h-5 w-5" />
-        <span className="sr-only">Playlists</span>
+        className="rounded-full"
+        onClick={() => {
+          window.localStorage.removeItem('key')
+          window.location.reload()
+        }}>
+        <KeyRound className="h-5 w-5" />
+        <span className="sr-only">Remove Key</span>
       </Button>
       <Button
+        title="Log Out"
         variant="ghost"
         size="icon"
-        className="rounded-full mt-auto">
-        <SettingsIcon className="h-5 w-5" />
+        className="rounded-full mt-auto"
+        onClick={() => {
+          window.localStorage.removeItem('refreshToken')
+          window.location.reload()
+        }}>
+        <LogOut className="h-5 w-5" />
         <span className="sr-only">Settings</span>
       </Button>
     </aside>
