7997b0088aa9928e3771d418e6e711ef9af4f0ed
Samuel Gomez <samuelgomez4010@gmail.com>
2025-01-20 18:38:40 -0500

    Add SearchBar component

---
+++ src/components/ui/SearchBar.tsx
@@ -0,0 +1,18 @@
+import { FaSearch } from 'react-icons/fa';
+
+interface Props {
+  placeholder: string;
+  className?: string;
+}
+export function SearchBar({ placeholder, className }: Props) {
+  return (
+    <div className={`relative  ${className}`}>
+      <FaSearch className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" />
+      <input
+        type="text"
+        placeholder={placeholder}
+        className="w-full bg-gray-800 text-white pl-10 p-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500"
+      />
+    </div>
+  );
+}
